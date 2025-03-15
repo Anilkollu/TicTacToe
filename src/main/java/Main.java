25d1be2dd7b39a39329b8206d8293d3e6eadbb54
Anilkollu <kolluanil555@gmail.com>
2025-03-15 18:17:49 +0000

    Updated with an Exception Handling case
    Added an Exception Handling for the case when an user enter index out of bound, will get and exception "Index out of bounds, please enter right index!"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,26 +16,35 @@ public class Main {
 
     while (!gameOver) {
       printBoard(board);
-      System.out.print("Player " + player + " enter: ");
-      int row = scanner.nextInt();
-      int col = scanner.nextInt();
-      System.out.println();
+      System.out.print("Player" + player + "enter: ");
+      try{
+        int row = scanner.nextInt();
+        int col = scanner.nextInt();
+        System.out.println();
 
-      if (board[row][col] == ' ') {
-        board[row][col] = player; 
-        gameOver = haveWon(board, player);
-        if (gameOver) {
-          System.out.println("Player " + player + " has won: ");
-        } else {
-          if (player == 'X') {
-          player = 'O';
-          } else {
-          player = 'X';
-          }
-          
+        // check if the indices are out of bound:
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) {
+            throw new ArrayIndexOutOfBoundsException("Index out of bounds, please enter right index!");
         }
-      } else {
-        System.out.println("Invalid move. Try again!");
+
+        if (board[row][col] == ' ') {
+          board[row][col] = player; 
+          gameOver = haveWon(board, player);
+          if (gameOver) {
+            System.out.println("Player " + player + " has won: ");
+          } else {
+            if (player == 'X') {
+            player = 'O';
+            } else {
+            player = 'X';
+            }
+          }
+        } else {
+          System.out.println("Invalid move. Try again!");
+        }
+      }
+      catch(ArrayIndexOutOfBoundsException e){
+        System.out.println(e.getMessage());
       }
     }
     printBoard(board);
@@ -45,19 +54,15 @@ public class Main {
     // check the rows
     for (int row = 0; row < board.length; row++) {
       if (board[row][0] == player && board[row][1] == player && board[row][2] == player) return true;
-      
     }
 
     // check for col
     for (int col = 0; col < board[0].length; col++) {
       if (board[0][col] == player && board[1][col] == player && board[2][col] == player) return true;
-
     }
 
     // diagonal
     if (board[0][0] == player && board[1][1] == player && board[2][2] == player)  return true;
-    
-
     if (board[0][2] == player && board[1][1] == player && board[2][0] == player)  return true;
     
     return false;
@@ -66,9 +71,17 @@ public class Main {
   public static void printBoard(char[][] board) {
     for (int row = 0; row < board.length; row++) {
       for (int col = 0; col < board[row].length; col++) {
-        System.out.print(board[row][col] + " | ");
+        System.out.print( "_" + board[row][col] + "_" + "|" );
+        
       }
       System.out.println();
     }
   }
 }
+
+
+
+
+
+
+
